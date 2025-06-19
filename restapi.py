7ae593a5446c5c0ec83a9f9ca89675b265c25d79
sthairno <45668272+sthairno@users.noreply.github.com>
2025-06-19 22:59:15 +0900

    update descriptions for openapi

--- restapi.py
+++ restapi.py
@@ -1,8 +1,9 @@
 import os
-from typing import List
+from typing import Annotated, List
 from fastapi import FastAPI
+from fastapi.params import Query
 from fastapi.responses import PlainTextResponse
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from starlette.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 
@@ -12,8 +13,14 @@ load_dotenv()
 MARKDOWN_DIR = os.environ["MARKDOWN_DIR"]
 EN_BASE_URL = os.environ["EN_BASE_URL"]
 JA_BASE_URL = os.environ["JA_BASE_URL"]
+OPENAPI_SERVER_URL = os.environ.get("SERVER_URL", "http://localhost")
 
-app = FastAPI()
+# Initialize cache
+markdown_cache = MarkdownCache(MARKDOWN_DIR)
+
+app = FastAPI(servers=[
+    {"url": OPENAPI_SERVER_URL}
+])
 
 app.add_middleware(
     CORSMiddleware,
@@ -23,30 +30,33 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Initialize cache
-markdown_cache = MarkdownCache(MARKDOWN_DIR)
-
 class SearchResult(BaseModel):
-    url_en: str
-    url_ja: str = None
-    content: str
-    score: float
+    url_en: str = Field(..., title="URL of the English documentation")
+    url_ja: str = Field(..., title="URL of the Japanese documentation")
+    content: str = Field(..., title="Full content of the matched document in Markdown format")
+    score: float = Field(..., title="Relevance score based on TF-IDF similarity")
 
 @app.post("/search")
-def search_docs(query: str, limit: int = 10) -> List[SearchResult]:
+def search_docs(
+    query: Annotated[
+        str,
+        Query(
+            ...,
+            title="Query",
+            description="Query string to search. Only English queries are supported"
+        )
+    ],
+    limit: Annotated[
+        int,
+        Query(
+            ...,
+            title="Limit",
+            description="Maximum number of results to return",
+        )
+    ] = 10
+) -> List[SearchResult]:
     """
     Search Siv3D documentation for a given query using TF-IDF similarity.
-
-    ## Parameters
-    - query (str): Query string to search. Only English queries are supported.
-    - limit (int): Maximum number of results (optional, default: 10)
-
-    ## Returns
-    - List: list of search results containing:
-      - url_en (str): URL of the English documentation
-      - url_ja (str): URL of the Japanese documentation
-      - content (str): Full content of the matched document in Markdown format
-      - score (float): Relevance score based on TF-IDF similarity
     """
 
     result = markdown_cache.search(query, limit)
